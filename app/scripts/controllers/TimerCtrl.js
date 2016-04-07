3d62e73a1756a73bc3ec7c11d5962cbf7428f27f
Sara Clark <sara.guittar@gmail.com>
2016-04-07 10:45:56 -0400

    styles updated and timer functional

--- app/scripts/controllers/TimerCtrl.js
+++ app/scripts/controllers/TimerCtrl.js
@@ -1,73 +1,119 @@
 (function() {
 	function TimerCtrl($scope, $interval, $filter) {
 
-		var regularTimer = 60 * 25; // 60s * 25m
-		var shortBreakTimer = 60 * 5; // 60s * 5m
-		var longBreakTimer = 60 * 30; // 60s * 30 m
-		var resetButton = $('#reset-button');
+		var regularTimer = 60 * .05; // 60s * 25m
+		var shortBreakTimer = 60 * .05; // 60s * 5m
+		var longBreakTimer = 60 * 0.1; // 60s * 30 m
+		var pomodorosPerLongBreak = 4; //4
+
 		var startButton = $('#start-button');
 		var breakButton = $('#break-button');
+		var onBreak = false;
 
-		resetButton.hide();
-		breakButton.hide();
+		var picturesDone = $('.done-pic'); // array-list of DOM class
+		var picturesBreak = $('.on-break-pic'); //array-list of DOM class
+		console.log(picturesDone instanceof HTMLCollection);
+		console.log(picturesDone instanceof NodeList);
+
+		picturesDone.hide(); // hide all pictures on load
+		picturesBreak.hide(); // hide all pictures on load
+
+		breakButton.hide(); // hide break button on load
 
 		var pomodoros = 0;
-		//when at 4 -> long break timer
 
 		$scope.time = regularTimer;
+
 		var countdown = null;
 
-		$scope.start = function() {
-			if (countdown !== null) {
-				return;
-			}
+		/** 
+		@function Start
+		@desc if countdown is not active, do nothing OR decrement time with interval
+		then stop at 0, show break button
+		**/
 
-			if ($scope.time !==0) {
-				countdown = $interval(function() {
-					$scope.time--;
-					if ($scope.time === 0) {
-						$scope.stop();
-					};
-				}, 1000);
-			};
-
-			startButton.hide();
-			resetButton.show();
-			breakButton.show();
-
-			pomodoros++;
-		};
-
-		$scope.reset = function() {
-			breakButton.hide();
-			if (countdown !== null) {
+		var tickFunction = function() {
+			//used for both work timer and break timer
+			$scope.time--;
+			if ($scope.time === 0) {
 				$scope.stop();
-			} 
-			$scope.time = 60 * 25;
-			$scope.start();
 
-			pomodoros++;
+				if(onBreak) {
+					//break just ended
+					//only choice from here is to restart session
+					onBreak = false;
+					startButton.show();
+					breakButton.hide();
+				} else {
+					//work session just ended
+					//choices are restart & break
+					startButton.show();
+					breakButton.show();
+					pomodoros++;
+					$('.container').addClass('finished');
+					//TODO show success image
+					picturesDone.show();
+				}
+
+			};
+		}
+
+		$scope.start = function() {
+			$('.container').removeClass('onBreak');
+			picturesDone.hide();
+			picturesBreak.hide();
+
+			$scope.time = regularTimer;
+			countdown = $interval(tickFunction, 1000);
+			startButton.hide();
+			breakButton.hide();
 		};
+
+
+		/**
+		@function Stop
+		@desc cancel the countdown and reset to null
+		**/
 
 		$scope.stop = function() {
 			$interval.cancel(countdown);
 			countdown = null;
 		};
 
+		/**
+		@function Break
+		@desc if pomodoros/sessions are 4 (or over) are longBreak, otherwise shortBreak
+		**/
+
 		$scope.break = function() {
-			if (pomodoros < 4) {
+			$('.container').removeClass('finished');
+			$('.container').addClass('onBreak');
+			onBreak = true;
+
+			//TODO show watchy image
+			picturesDone.hide();
+			picturesBreak.show();
+			if (pomodoros < pomodorosPerLongBreak) {
 				$scope.time = shortBreakTimer;
-				$scope.start();
-				breakButton.hide();
-			} else if (pomodoros >= 4) {
+			} else {
 				$scope.time = longBreakTimer;
-				$scope.start();
-				breakButton.hide();
-				pomodoros = 0;
-				startButton.show();
+				pomodoros = 0; // reset pomodoros to 0 after long break
+			}
+			countdown = $interval(tickFunction, 1000);
+			startButton.hide();
+			breakButton.hide();
+		};
+
+		var imageCycle = function(list) {
+			list.hide();
+			for (var i = 0; i < list.length; i++) {
+				var image = list[i];
+				image.show();
 			}
 		};
 	}
+
+	//start (working) break stopped
 
 	angular
 		.module('app')
